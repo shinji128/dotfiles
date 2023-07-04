3e7a8a4536da0a40cb687357785b8f694a9ca467
miura <shinji65536@gmail.com>
2023-07-04 20:17:10 +0900

    fix-lint

--- .config/nvim/lua/pluginconfig/toggleterm.lua
+++ .config/nvim/lua/pluginconfig/toggleterm.lua
@@ -1,12 +1,12 @@
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({
-	cmd = "lazygit",
-	direction = "float",
-	hidden = true
+  cmd = "lazygit",
+  direction = "float",
+  hidden = true
 })
 
 function _lazygit_toggle()
-	lazygit:toggle()
+  lazygit:toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<space>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true })
