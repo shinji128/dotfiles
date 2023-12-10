4d5c86b8f14cc51817643a0c7f6af61af57565fb
miura <shinji65536@gmail.com>
2023-12-10 15:08:31 +0900

    wezterm導入

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+return {
+  color_scheme = 'Catppuccin Mocha',
+  window_background_opacity = 0.93,
+  font = require("wezterm").font("cica"),
+  font_size = 16.0,
+  window_frame = {
+    font_size = 14.0,
+  },
+  leader = { key = 'f', mods = 'CTRL', timeout_milliseconds = 2000 },
+}
