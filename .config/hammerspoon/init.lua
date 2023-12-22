f7edab0666aba1690d4cff19473229dc0cc7d539
miura <shinji65536@gmail.com>
2023-12-22 19:23:11 +0900

    hammerspoonのリファクタリング

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,24 +1,24 @@
 function toggleApp(appName)
-    local app = hs.application.get(appName)
+  local app = hs.application.get(appName)
 
-    if app == nil or app:isHidden() then
-        hs.application.launchOrFocus(appName)
-    else
-        app:hide()
-    end
+  if app == nil or app:isHidden() then
+    hs.application.launchOrFocus(appName)
+  else
+    app:hide()
+  end
 end
 
-local appBindings = {
-    { key = "v", appName = "Alacritty" },
-    { key = "space", appName = "WezTerm" },
-    { key = "s", appName = "Slack" },
-    { key = "g", appName = "Clock" },
-    { key = "f", appName = "Notion" },
-    { key = "b", appName = "Arc.app" }
+local appBinds = {
+  { prefix = { 'alt' }, key = "v", appName = "Alacritty" },
+  { prefix = { 'alt' }, key = "space", appName = "WezTerm" },
+  { prefix = { 'alt' }, key = "s", appName = "Slack" },
+  { prefix = { 'alt' }, key = "g", appName = "Clock" },
+  { prefix = { 'alt' }, key = "f", appName = "Notion" },
+  { prefix = { 'alt' }, key = "b", appName = "Arc.app" }
 }
 
-for _, binding in ipairs(appBindings) do
-    hs.hotkey.bind({ "alt" }, binding.key, function()
-        toggleApp(binding.appName)
-    end)
+for _, appBind in ipairs(appBinds) do
+  hs.hotkey.bind(appBind.prefix, appBind.key, function()
+    toggleApp(appBind.appName)
+  end)
 end
