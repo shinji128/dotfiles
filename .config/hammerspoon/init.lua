8430138fc627ac9bf47a23f54bb42ea0309429fe
miura <shinji65536@gmail.com>
2024-01-10 14:39:37 +0900

    hotkeyにdockerdesktop追加

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -15,7 +15,8 @@ local appBinds = {
   { prefix = { 'alt' }, key = "t", appName = "Clock" },
   { prefix = { 'alt' }, key = "n", appName = "Notion" },
   { prefix = { 'alt' }, key = "a", appName = "Arc.app" },
-  { prefix = { 'alt' }, key = "p", appName = "Postman" }
+  { prefix = { 'alt' }, key = "p", appName = "Postman" },
+  { prefix = { 'alt' }, key = "d", appName = "Docker Desktop" }
 }
 
 for _, appBind in ipairs(appBinds) do
