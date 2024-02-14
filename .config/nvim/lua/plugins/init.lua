91eac87dbd8a57b5e44078a987e08c28d120241b
miura <shinji65536@gmail.com>
2024-02-14 20:47:58 +0900

    pluginアップデート

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     event = "VimEnter",
-    branch = "v2.x",
+    branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
