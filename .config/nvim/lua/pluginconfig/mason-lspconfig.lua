5a028ff9cce8bfcb6e041b460fe8859be7e349bf
miura <shinji65536@gmail.com>
2024-06-18 09:13:33 +0900

    mason-lspconfigからsolargraphを削除

--- .config/nvim/lua/pluginconfig/mason-lspconfig.lua
+++ .config/nvim/lua/pluginconfig/mason-lspconfig.lua
@@ -2,7 +2,7 @@ local mason_lspconfig = require("mason-lspconfig")
 
 -- mason-lspconfigの設定
 mason_lspconfig.setup({
-  ensure_installed = { "tsserver", "lua_ls", "solargraph" }, -- インストールするLSPサーバー
+  ensure_installed = { "tsserver", "lua_ls" }, -- インストールするLSPサーバー
 })
 
 -- 各LSPサーバーの設定
@@ -13,6 +13,3 @@ lspconfig.tsserver.setup{}
 
 -- LuaのLSPサーバー設定
 lspconfig.lua_ls.setup{}
-
--- RubyのLSPサーバー設定
-lspconfig.solargraph.setup{}
