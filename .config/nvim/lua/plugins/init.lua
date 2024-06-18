bd8f0161a239f8bc91f8c86ab980c14f1ec71be3
miura <shinji65536@gmail.com>
2024-06-18 09:11:59 +0900

    neotreeの不要なコメント削除

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,32 +1,26 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    event = "VimEnter",
     branch = "v3.x",
+    event = "VimEnter",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       {
-        -- only needed if you want to use the commands with "_with_window_picker" suffix
         's1n7ax/nvim-window-picker',
-        -- tag = "v1.*",
+        version = '2.*',
         config = function()
-        require 'window-picker'.setup({
-          autoselect_one = true,
-          include_current = false,
-          filter_rules = {
-            -- filter using buffer options
-            bo = {
-              -- if the file type is one of following, the window will be ignored
-              filetype = { 'neo-tree', "neo-tree-popup", "notify" },
-
-              -- if the buffer type is one of following, the window will be ignored
-              buftype = { 'terminal', "quickfix" },
+          require 'window-picker'.setup({
+            filter_rules = {
+              include_current_win = false,
+              autoselect_one = true,
+              bo = {
+                filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+                buftype = { 'terminal', "quickfix" },
+              },
             },
-          },
-          other_win_hl_color = '#e35e4f',
-        })
+          })
         end,
       },
     },
