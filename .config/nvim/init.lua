7b4c68c740816823c0d1c2313572aa514aa2d3b4
miura <shinji65536@gmail.com>
2024-08-04 22:15:09 +0900

    lazy.nvimのアップデートに合わせて設定修正

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,27 +1,31 @@
-require('options')
-require('keymaps')
+require("luarc")
+require("options")
+require("keymaps")
 
 -- lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-      'git',
-      'clone',
-      '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable',
-      lazypath,
-  })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     spec = {
-        { import = "plugins" },
+      { import = "plugins" },
     },
     defaults = {
-        lazy = true,
-        version = "*",
+      lazy = true,
+      -- version = "*",
     },
     ui = {
         icons = {
