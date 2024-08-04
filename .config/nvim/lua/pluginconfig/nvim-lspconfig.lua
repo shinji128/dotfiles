40505eb8e2227ce9e8bbcf6e9abfc9ff908c4c00
miura <shinji65536@gmail.com>
2024-08-04 22:19:00 +0900

    fix-nvim-lspconfig

--- .config/nvim/lua/pluginconfig/nvim-lspconfig.lua
+++ .config/nvim/lua/pluginconfig/nvim-lspconfig.lua
@@ -1,9 +1,9 @@
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "ruby",
---   callback = function()
---     vim.lsp.start {
---       name = "rubocop",
---       cmd = { "bundle", "exec", "rubocop", "--lsp" }
---     }
---   end,
--- })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  callback = function()
+    vim.lsp.start {
+      name = "rubocop",
+      cmd = { "bundle", "exec", "rubocop", "--lsp" }
+    }
+  end,
+})
