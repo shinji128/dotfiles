a00b50c47b5e2223ee35268c2eafae3515384d56
miura <shinji65536@gmail.com>
2024-09-30 16:01:37 +0900

    add: rust_analyzer

--- .config/nvim/lua/pluginconfig/mason-lspconfig.lua
+++ .config/nvim/lua/pluginconfig/mason-lspconfig.lua
@@ -2,7 +2,7 @@ local mason_lspconfig = require("mason-lspconfig")
 
 -- mason-lspconfigの設定
 mason_lspconfig.setup({
-  ensure_installed = { "tsserver", "lua_ls" }, -- インストールするLSPサーバー
+  ensure_installed = { "tsserver", "lua_ls", "rust_analyzer" }, -- インストールするLSPサーバー
 })
 
 -- 各LSPサーバーの設定
@@ -13,3 +13,5 @@ lspconfig.tsserver.setup{}
 
 -- LuaのLSPサーバー設定
 lspconfig.lua_ls.setup{}
+
+lspconfig.rust_analyzer.setup{}
