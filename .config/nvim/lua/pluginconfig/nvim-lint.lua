e9b2d508db7c9f16b43ed13c61bc237bcf2cc73e
miura <shinji65536@gmail.com>
2024-11-11 14:34:45 +0900

    delete: nvim-lint

--- .config/nvim/lua/pluginconfig/nvim-lint.lua
+++
@@ -1,9 +0,0 @@
-local lint = require("lint")
-lint.linters_by_ft = {
-  ruby = {'rubocop'},
-}
-vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
-  callback = function()
-    lint.try_lint()
-  end,
-})
