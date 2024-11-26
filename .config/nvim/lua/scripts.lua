1ab830c800820c4633d1b094a4880f8cf1a1ee8c
miura <shinji65536@gmail.com>
2024-11-26 09:27:45 +0900

    fix: scripts ファイル名とファイルパスをクリップボードにコピーするように修正

--- .config/nvim/lua/scripts.lua
+++ .config/nvim/lua/scripts.lua
@@ -4,18 +4,20 @@ function M.get_relative_file_path()
   local absolute_file_path = vim.api.nvim_buf_get_name(0)
   local git_path = vim.fn.system("git rev-parse --show-toplevel")
 
-  local relative_file_path = "/" .. string.sub(absolute_file_path, git_path:len() + 1)
-  local copy_command = "echo " .. "\"." .. relative_file_path .. "\"" .. " | pbcopy"
-  vim.fn.system(copy_command)
-  print(relative_file_path)
-  print("command:" .. copy_command)
+  local relative_file_path = string.sub(absolute_file_path, git_path:len() + 1)
+  vim.fn.setreg('+', relative_file_path)
+  -- vimのレジスタのみにコピーする時に使用
+  -- local copy_command = "echo " .. "\"" .. relative_file_path .. "\"" .. " | pbcopy"
+  -- vim.fn.system(copy_command)
   return true
 end
 
 function M.get_file_name()
   local file_name = vim.fn.expand('%:t')
-  local copy_command = 'echo "' .. file_name .. '" | pbcopy'
-  vim.fn.system(copy_command)
+  vim.fn.setreg('+', file_name)
+  -- vimのレジスタのみにコピーする時に使用
+  -- local copy_command = 'echo "' .. file_name .. '" | pbcopy'
+  -- vim.fn.system(copy_command)
   return true
 end
 
