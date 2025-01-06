7430d05b84798f11d5a65469e90c2404e1aaa87d
miura <shinji65536@gmail.com>
2025-01-06 15:27:55 +0900

    fix: telescope kyebind

--- .config/nvim/lua/pluginconfig/telescope.lua
+++ .config/nvim/lua/pluginconfig/telescope.lua
@@ -1,4 +1,14 @@
+local actions = require("telescope.actions")
+
 require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-s>"] = actions.select_horizontal,
+        ["<C-v>"] = actions.select_vertical,
+      },
+    },
+  },
   extensions = {
     fzf = {
       fuzzy = true,
